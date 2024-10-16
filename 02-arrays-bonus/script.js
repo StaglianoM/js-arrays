1b5051ebc32101e8385cdcbbed2dd900163a60fc
StaglianoM <stagliano.m92@gmail.com>
2024-10-16 17:10:27 +0200

    2°  complete

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -6,14 +6,21 @@ const teachers = [
   'Carlo',
   'Lewis',
   'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+]; 
+console.log(teachers);// NON MODIFICARE QUESTA VARIABILE
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
+console.log(teachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(teacher => teacher.length >= 5);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const removeName = teachers.indexOf('Ed');
+console.log(removeName);
+teachers.splice(removeName, 1);
+console.log(teachers);
